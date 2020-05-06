fb0568133c05aa7631964c798e14d48e4729cdb4
Yuri Almeida <yurialves2@gmail.com>
2020-05-06 13:59:33 -0300

    2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const Ola = () => <h1>Olá, Mundo!</h1>;
+const Ola = () => <h1>Olá, Mundo! 2</h1>;
 
 render(<Ola />, document.querySelector('#app'));
