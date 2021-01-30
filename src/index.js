aea843d122209cccf07d11672a7c7165ac876c40
Yuri Almeida <yurialves2@gmail.com>
2021-01-30 17:45:13 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-const Ola = () => <h1>Olá, Mundo! 2</h1>;
+const Ola = () => <h1>Olá, Mundo! teste</h1>;
 
-render(<Ola />, document.querySelector('#app'));
+render(<Ola />, document.querySelector("#app"));
